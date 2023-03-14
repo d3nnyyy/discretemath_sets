02d1173db2dc5120409db41f9b009abe5ebee929
Denys <116681720+d3nnyyy@users.noreply.github.com>
2023-03-14 19:38:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,25 @@ form1.addEventListener('submit', event => {
     const input1 = document.querySelector('#array-input1');
     const arrayInputValue1 = input1.value.trim();
     const array1 = arrayInputValue1.split(/\s+/).map(item => parseInt(item.trim())).filter(item => !isNaN(item));
-    const uniqueArray = [...new Set(array1)];
+    // const uniqueArray = [...new Set(array1)];
+
+    let tempArray = Array(array1.length).fill(0);
+    let k = 0;
+    for (let i = 0; i < array1.length; i++) {
+        let isDuplicate = false;
+        for (let j = 0; j < k; j++) {
+            if (array1[i] === tempArray[j]) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (!isDuplicate) {
+            tempArray[k] = array1[i];
+            k++;
+        }
+    }
+    const uniqueArray = tempArray.filter(item => item !== 0);
+
     uniqueElementsResult1.textContent = `Результат: [${uniqueArray.join(', ')}]`;
 });
 
@@ -21,10 +39,13 @@ form2.addEventListener('submit', event => {
     const array2 = arrayInputValue2.split(/\s+/).map(item => parseInt(item.trim())).filter(item => !isNaN(item));
     const array3 = arrayInputValue3.split(/\s+/).map(item => parseInt(item.trim())).filter(item => !isNaN(item));
     const cartesianProduct = [];
+    let index = 0;
     for (let i = 0; i < array2.length; i++) {
         for (let j = 0; j < array3.length; j++) {
-            cartesianProduct.push([array2[i], array3[j]]);
+            // cartesianProduct.push([array2[i], array3[j]]);
+            cartesianProduct[index] = [array2[i], array3[j]];
+            index++;
         }
     }
-    uniqueElementsResult2.textContent = `Декартовий добуток: [${cartesianProduct.join(', ')}]`;
+    uniqueElementsResult2.textContent = `Декартовий добуток: [${cartesianProduct.join(' ; ')}]`;
 });
